962bcb894c55122e8a0919b29c0aad2a8b5c78d8
HAITAM_ELIMAMI <haitamelimami1998@outlook.fr>
2023-02-21 14:52:24 -0800

    7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,26 +8,17 @@
  */
 int print_last_digit(int n)
 {
-	int nm;
-	int nc;
-	int nd;
+	int l;
 
-	if (n <= -1000 || n >= 1000)
+	l = n % 10;
+	if (l < 0)
 	{
-		nm = n % 1000;
-		nc = nm % 100;
-		nd = nc % 10;
-		return (nd);
-	}
-	else if (n <= 100 || n >= 100)
-	{
-		nc = n % 100;
-		nd = nc % 10;
-		return (nd);
+		_putchar(-l + 48);
+		return (-l);
 	}
 	else
 	{
-		nd = n % 10;
-		return (nd);
+		_putchar(l + 48);
+		return (l);
 	}
 }
