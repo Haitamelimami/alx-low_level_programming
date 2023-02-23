1b98ba659079c2d306890329502e0d1f441dfbb5
HAITAM_ELIMAMI <haitamelimami1998@outlook.fr>
2023-02-23 10:01:43 -0800

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,14 +4,14 @@
  *
  * Return: void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int a;
 
 	for (a = 0; a <= 9; a++)
 	{
 		if (a != 2 || a != 4)
-		{	
+		{
 			_putchar(a + '0');
 		}
 	}
