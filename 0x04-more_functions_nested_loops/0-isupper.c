541962d59b74272d74c85f7c3ce9c403cee33fff
HAITAM_ELIMAMI <haitamelimami1998@outlook.fr>
2023-02-23 09:30:35 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper(int c)- checks for uppercase character
+ *
+ * @c: entry to be tested
+ *
+ * Return: 1 if true 0 if false
+ */
+int _isupper(int c)
+{
+	if (c < 91 && c > 64)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+)
