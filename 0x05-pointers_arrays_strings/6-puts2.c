cc54608eba5c7bf55c08e339bb5441deaeba547f
HAITAM_ELIMAMI <haitamelimami1998@outlook.fr>
2023-02-27 09:13:41 -0800

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2(char *str)- prints every other character of a string
+ *
+ * @str: entry str
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int l = 0;
+	char *y = str;
+	int z;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for (z = 0 ; z <= l ; z++)
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+	}
+	_putchar('\n');
+}
