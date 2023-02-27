b63931a8cf2c896250bca05191cd512055849997
HAITAM_ELIMAMI <haitamelimami1998@outlook.fr>
2023-02-27 08:29:30 -0800

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen(char *s)- returns the length of a string
+ *
+ * @s:entry char
+ *
+ * Return: lenth
+ */
+int _strlen(char *s)
+{
+	int lenth = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
